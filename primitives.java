fde12ebc21c6bc56ce33241587fc1c2f9e6a8865
mlatib023 <mlatib023@student.wethinkcode.co.za>
2024-10-02 12:01:52 +0200

    data types

---
+++ primitives.java
@@ -0,0 +1,23 @@
+/*
+ Primitive data types in java:
+ * int
+ * float
+ * char
+ 
+ Other data types in java:
+ * Array
+ * string
+ * arrayList
+ * linkedList
+ * hashMap
+ * hashSet
+ * treeSet
+ * stack
+ * queue
+ * Priority Queue
+ * Deque
+ * linkedHashMap
+ * TreeMap
+ * enumSet
+ * bitSet
+ */
